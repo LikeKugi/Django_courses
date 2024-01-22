d629d1148c307f22cb0743eac6cd6b0a87cbeec2
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2024-01-22 20:05:49 +0300

    feat: add urls

--- selfedu/women/views.py
+++ selfedu/women/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Women application page')
+
+
+def categories(request):
+    return HttpResponse('<h1>Articles by categories</h1>')
