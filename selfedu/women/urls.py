0f91293145c401a36ec05e1149c0b184eeec7617
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2024-01-23 18:57:13 +0300

    feat: add converters

--- selfedu/women/urls.py
+++ selfedu/women/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 
 from . import views
+from . import converters
+
+register_converter(converters.TwoDigitMonthConverter, 'month')
 
 urlpatterns = [
     path('', views.index),
-    path('cat/', views.categories)
+    path('cat/<int:cat_id>/', views.categories),
+    path('cat/<slug:cat_slug>/', views.categories_by_slug),
+    re_path(r'^archive/(?P<year>[0-9]{4})/', views.archive),
+    path('archive/<month:month>/', views.archive_by_month),
+
 ]
--- selfedu/women/views.py
+++ selfedu/women/views.py
@@ -1,11 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
     return HttpResponse('Women application page')
 
 
-def categories(request):
-    return HttpResponse('<h1>Articles by categories</h1>')
+def categories(request, cat_id: int):
+    return HttpResponse(f'<h1>Articles by categories</h1><p>ID: {cat_id}</p>')
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f'<h1>Articles by slug</h1><p>Slug: {cat_slug}</p>')
+
+
+def archive(request, year):
+    return HttpResponse(f'<h1>Archive by years</h1><p>Year: {year}</p>')
+
+
+def archive_by_month(request, month):
+    return HttpResponse(f'<h1>Archive by years</h1><p>Month: {month}</p>')
