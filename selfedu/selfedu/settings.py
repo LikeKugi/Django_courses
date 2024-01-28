5018f3e23e2ec5b748676b209633feffe75740d7
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2024-01-28 15:00:38 +0300

    feat: add static files

--- selfedu/selfedu/settings.py
+++ selfedu/selfedu/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)ksa9v1t_bd*ol=-r*m3f5gts0uu=f)gb54ss$cw91^evy^cq4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = (BASE_DIR / 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
